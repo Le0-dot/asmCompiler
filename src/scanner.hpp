93841908b4e4661a8d1c229b4dd254f35348985d
Le0-dot <lkolyadich@gmail.com>
2022-03-02 15:45:38 +0100

    Finished scanner class and add some utils

--- src/scanner.hpp
+++ src/scanner.hpp
@@ -5,15 +5,18 @@
 #include <map>
 #include <functional>
 #include <stdexcept>
+#include <memory>
+#include <cctype>
 
 #include "word/words.hpp"
 #include "utils.hpp"
 
+template<typename num>
 class scanner
 {
     private:
 	using iqueue = std::queue<char>;
-	using oqueue = std::queue<word*>;
+	using oqueue = std::queue<std::shared_ptr<word>>;
 
 	iqueue& input_queue;
 	oqueue& output_queue;
@@ -46,200 +49,255 @@ class scanner
 		char current = pop_queue(input_queue);
 		if(current == ' ')
 		    continue;
-		prev = funcs[current](prev);
+		else if(current == '\n') {
+		    output_queue.push(std::make_shared<endline_word>());
+		    std::cerr << "output_queue.push(std::make_shared<endline_word>())" << std::endl;
+		} else if(std::isdigit(current))
+		    prev = num_char(prev, current);
+		else
+		    prev = funcs[current](prev);
 	    }
 	}
 
     private:
-	std::string a_char(std::string& prev)
+	std::string a_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::a_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
+	    std::cerr << "scanner::a_char(\"" << prev << "\") [input_queue.front() = '" 
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
 	    if(prev == "" && (input_queue.front() != 'd' && input_queue.front() != 'n'))
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 'a');
 	    if(prev == "tr" && input_queue.front() != 'p')
-		throw std::runtime_error(prev);
-	    if(prev == "le" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::lea));
+		throw std::runtime_error(prev + 'a');
+	    if(prev == "le" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::lea));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
 	    if(prev == "h" && input_queue.front() != 'l')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 'a');
 	    return prev + 'a';
 	}
 
-	std::string d_char(std::string& prev)
+	std::string d_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::d_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
+	    std::cerr << "scanner::d_char(\"" << prev << "\") [input_queue.front() = '"
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
 	    if(prev == "a" && input_queue.front() != 'd')
-		throw std::runtime_error(prev);
-	    if(prev == "ad" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::add));
+		throw std::runtime_error(prev + 'd');
+	    if(prev == "ad" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::add));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
-	    if(prev == "an" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::and_op));
+	    if(prev == "an" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::and_op));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
-	    if(prev == "l" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::ld));
+	    if(prev == "l" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::ld));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
 	    if(prev == "l" && (input_queue.front() != 'i' && input_queue.front() != 'r'))
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 'd');
 	    return prev + 'd';
 	}
 
-	std::string n_char(std::string& prev)
+	std::string n_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::n_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
+	    std::cerr << "scanner::n_char(\"" << prev << "\") [input_queue.front() = '" 
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
 	    if(prev == "" && input_queue.front() != 'o')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 'n');
 	    if(prev == "a" && input_queue.front() != 'd')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 'n');
 	    return prev + 'n';
 	}
 
-	std::string l_char(std::string& prev)
+	std::string l_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::l_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
+	    std::cerr << "scanner::l_char(\"" << prev << "\") [input_queue.front() = '" 
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
 	    if(prev == "" && (input_queue.front() != 'd' &&
 			      input_queue.front() != 'e'))
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 'l');
 	    if(prev == "ha" && input_queue.front() != 't')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 'l');
 	    return prev + 'l';
 	}
 
-	std::string i_char(std::string& prev)
+	std::string i_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::i_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
-	    if(prev == "ld" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::ldi));
+	    std::cerr << "scanner::i_char(\"" << prev << "\") [input_queue.front() = '" 
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
+	    if(prev == "ld" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::ldi));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
-	    if(prev == "st" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::sti));
+	    if(prev == "st" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::sti));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
 	    return prev + 'i';
 	}
 
-	std::string r_char(std::string& prev)
+	std::string r_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::r_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
-	    if(prev == "ld" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::ldr));
+	    std::cerr << "scanner::r_char(\"" << prev << "\") [input_queue.front() = '" 
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
+	    if(prev == "ld" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::ldr));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
-	    if(prev == "st" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::str));
+	    if(prev == "st" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::str));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
-	    if(prev == "js" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::jsr));
+	    if(prev == "js" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::jsr));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
-	    if(prev == "b" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::br));
+	    if(prev == "b" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::br));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
 	    if(prev == "t" && input_queue.front() != 'a')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 'r');
 	    return prev + 'r';
 	}
 
-	std::string e_char(std::string& prev)
+	std::string e_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::e_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
+	    std::cerr << "scanner::e_char(\"" << prev << "\") [input_queue.front() = '" 
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
 	    if(prev == "l" && input_queue.front() != 'a')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 'e');
 	    return prev + 'e';
 	}
 
-	std::string o_char(std::string& prev)
+	std::string o_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::o_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
+	    std::cerr << "scanner::o_char(\"" << prev << "\") [input_queue.front() = '" 
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
 	    if(prev == "n" && input_queue.front() != 't')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 'o');
 	    return prev + 'o';
 	}
 
-	std::string t_char(std::string& prev)
+	std::string t_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::t_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
-	    if(prev == "no" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::not_op));
+	    std::cerr << "scanner::t_char(\"" << prev << "\") [input_queue.front() = '" 
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
+	    if(prev == "no" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::not_op));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
-	    if(prev == "s" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::st));
+	    if(prev == "s" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::st));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
-	    if(prev == "hal" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::halt));
+	    if(prev == "hal" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::halt));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
 	    if(prev == "s" && input_queue.front() != 'i' && 
 		    input_queue.front() != 'r' && input_queue.front() != ' ')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 't');
 	    if(prev == "" && input_queue.front() != 'r')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 't');
 	    return prev + 't';
 	}
 
-	std::string s_char(std::string& prev)
+	std::string s_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::s_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
+	    std::cerr << "scanner::s_char(\"" << prev << "\") [input_queue.front() = '" 
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
 	    if(prev == "" && input_queue.front() != 't')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 's');
 	    if(prev == "j" && input_queue.front() != 'r')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 's');
 	    return prev + 's';
 	}
 
-	std::string j_char(std::string& prev)
+	std::string j_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::j_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
+	    std::cerr << "scanner::j_char(\"" << prev << "\") [input_queue.front() = '" 
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
 	    if(prev == "" && input_queue.front() != 'm' && input_queue.front() != 's')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 'j');
 	    return prev + 'j';
 	}
 
-	std::string m_char(std::string& prev)
+	std::string m_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::m_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
+	    std::cerr << "scanner::m_char(\"" << prev << "\") [input_queue.front() = '" 
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
 	    if(prev == "j" && input_queue.front() != 'p')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 'm');
 	    return prev + 'm';
 	}
 
-	std::string p_char(std::string& prev)
+	std::string p_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::p_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
-	    if(prev == "jm" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::jmp));
+	    std::cerr << "scanner::p_char(\"" << prev << "\") [input_queue.front() = '" 
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
+	    if(prev == "jm" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::jmp));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
-	    if(prev == "tra" && input_queue.front() == ' ') {
-		output_queue.push(new operation_word(operations::trap));
+	    if(prev == "tra" && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<operation_word>(operations::trap));
+		std::cerr << "output_queue.push(std::make_shared<operation_word>(operations::" << output_queue.back() << "))" << std::endl;
 		return "";
 	    }
 	    return prev + 'p';
 	}
 
-	std::string b_char(std::string& prev)
+	std::string b_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::b_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
+	    std::cerr << "scanner::b_char(\"" << prev << "\") [input_queue.front() = '" 
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
 	    if(prev == "" && input_queue.front() != 'r')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 'b');
 	    return prev + 'b';
 	}
 
-	std::string h_char(std::string& prev)
+	std::string h_char(const std::string& prev)
 	{
-	    std::cerr << "scanner::h_char(\"" << prev << "\") [input_queue.front() = '" << input_queue.front() << "']" << std::endl;
+	    std::cerr << "scanner::h_char(\"" << prev << "\") [input_queue.front() = '" 
+		<< (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
 	    if(prev == "" && input_queue.front() != 'a')
-		throw std::runtime_error(prev);
+		throw std::runtime_error(prev + 'h');
 	    return prev + 'h';
+	}
+
+	std::string num_char(const std::string& prev, char current)
+	{
+	    std::cerr << "scanner::num_char(\"" << prev << "\", '" << current 
+		<< "') [input_queue.front() = '" << (input_queue.front() == '\n' ? '\0' : input_queue.front()) << "']" << std::endl;
+	    if(prev == "r" && std::isspace(input_queue.front()) && current != '8' && current != '9') {
+		output_queue.push(std::make_shared<register_word>(ntor(current)));
+		std::cerr << "output_queue.push(std::make_shared<register_word>(registers::" << output_queue.back() << "))" << std::endl;
+		return "";
+	    }
+	    if((prev == "" || isnumber(prev)) && std::isspace(input_queue.front())) {
+		output_queue.push(std::make_shared<number_word<num>>(std::stoul(prev + current)));
+		std::cerr << "output_queue.push(std::make_shared<number_word<" << ttos<num>::v << ">>(" << output_queue.back() << "))" << std::endl;
+		return "";
+	    }
+	    if((prev == "" || isnumber(prev)) && !std::isdigit(input_queue.front()) && !std::isspace(input_queue.front()))
+		throw std::runtime_error(prev + current);
+	    return prev + current;
 	}
 };
--- src/utils.hpp
+++ src/utils.hpp
@@ -3,6 +3,10 @@
 #include <queue>
 #include <stdexcept>
 #include <cstring>
+#include <cctype>
+#include <cstdint>
+
+#include "word/words.hpp"
 
 template<typename T>
 T pop_queue(std::queue<T>& queue)
@@ -12,11 +16,11 @@ T pop_queue(std::queue<T>& queue)
     return val;
 }
 
-void error_handling(std::runtime_error& e, std::queue<char>& input_queue)
+void error_handling(const std::runtime_error& e, std::queue<char>& input_queue)
 {
     std::cout << "Syntax error: " << std::endl << e.what();
     char current;
-    while((current = pop_queue(input_queue)) != '\n')
+    while(!input_queue.empty() && (current = pop_queue(input_queue)) != '\n')
 	std::cout << current;
     std::cout << std::endl;
     for(int i = 0; i < strlen(e.what()) - 1; ++i)
@@ -24,8 +28,32 @@ void error_handling(std::runtime_error& e, std::queue<char>& input_queue)
     std::cout << '^' << std::endl;
 }
 
-void push_string_to_queue(std::string str, std::queue<char>& queue)
+bool isnumber(const std::string& str)
 {
+    bool res = true;
     for(const auto& c: str)
-	queue.push(c);
+	res = res && std::isdigit(c);
+    return res;
 }
+
+std::ostream& operator<<(std::ostream& out, std::shared_ptr<word> w)
+{
+    switch(w->get()) {
+	case words::operations:
+	    out << otos(static_cast<operation_word*>(w.get())->get_op());
+	    break;
+	case words::registers:
+	    out << rtos(static_cast<register_word*>(w.get())->get_reg());
+	    break;
+	case words::numbers:
+	    out << static_cast<number_word<uint16_t>*>(w.get())->get_num();
+	    break;
+    }
+    return out;
+}
+
+template<typename T>
+struct ttos { static constexpr auto v = ""; };
+
+template<>
+struct ttos<uint16_t> { static constexpr auto v = "uint16_t"; };
